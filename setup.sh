2061c2cead4218996f122db4244b31c3d5199d0c
Hiu Yen Onn <yenonn@gmail.com>
2020-05-21 14:45:44 +0800

    fix the vimrc commenting

--- setup.sh
+++ setup.sh
@@ -11,14 +11,14 @@ ln -snf $TEMPLATES_DIR ~/.vim/templates
 if [ -f ~/.vimrc ]
 then
   cat <<-EOF >> ~/.vimrc
-## k8s-template starts here
+"" k8s-template starts here
 :autocmd BufNewFile [P|p]od*.yaml 0r ~/.vim/templates/pod-definition.yaml
 :autocmd BufNewFile [R|r]c*.yaml 0r ~/.vim/templates/rc-definition.yaml
 :autocmd BufNewFile [R|r]s*.yaml 0r ~/.vim/templates/rs-definition.yaml
 :autocmd BufNewFile [D|d]eploy*.yaml 0r ~/.vim/templates/deployment-definition.yaml
 :autocmd BufNewFile [N|n]amespace*.yaml 0r ~/.vim/templates/namespace-definition.yaml
 :autocmd BufNewFile [S|s]ervice*.yaml 0r ~/.vim/templates/service-definition.yaml
-## k8s-template ends here
+"" k8s-template ends here
 EOF
 
 fi
